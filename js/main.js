3bcffb495e49d8d08f5b48a4b8be1936b225e7dc
Kolapo Oni <onikolapo@gmail.com>
2018-08-30 14:17:05 +0100

    added new pages

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
+var bgImageArray = ["blog-bg.png", "main-bg.png"],
+base = "img/",
+secs = 4;
+
+bgImageArray.forEach(function(img){
+    new Image().src = base + img; 
+    // caches images, avoiding white flash between background replacements
+});
+
+function backgroundSequence() {
+	window.clearTimeout();
+	var k = 0;
+	for (i = 0; i < bgImageArray.length; i++) {
+		setTimeout(function(){ 
+            document.getElementById("hero-image").Image.src = "url(" + base + bgImageArray[k] + ") no-repeat center center fixed";
+			// document.documentElement.style.background = "url(" + base + bgImageArray[k] + ") no-repeat center center fixed";
+			// document.documentElement.style.backgroundSize ="cover";
+		if ((k + 1) === bgImageArray.length) { setTimeout(function() { backgroundSequence() }, (secs * 1000))} else { k++; }			
+		}, (secs * 1000) * i)	
+	}
+}
+
+backgroundSequence();
+
+
 function goToSection(section){
     if(section == "blog"){
         window.location.href = "blog.html";
